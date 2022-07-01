24f7d630b661dac3dd6299bd1abae0baa3cc4d33
Ryan Tang <ryan.tang.clu@gmail.com>
2022-07-01 01:41:08 -0400

    login component working

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import ChatRoom from './components/navbar/chat/ChatRoom';
 import Profile from './components/profile/Profile';
-import SignIn from './components/authenticate/SignIn';
+// import SignIn from './components/authenticate/SignIn';
 import SignUp from './components/authenticate/SignUp';
+import Login from './components/authenticate/Login';
 import Maps from './components/Maps/Maps';
 import Home from './components/home/Home';
 
@@ -28,8 +29,8 @@ function App() {
         <Router>
           <Routes>
             <Route path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<SignUp />} />
-            <Route path='/signin' element={<SignIn />} />
           </Routes>
         </Router>
       </AuthProvider>
