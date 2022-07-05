7e5435f9b0bf911fb2e7e0a5e4eb9f7752007364
Roy Neville <royneville@Roys-MBP.attlocal.net>
2022-07-05 15:11:59 -0700

    Edit Post funcionality started

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
-import { collection, query, getDocs, getDoc, doc, orderBy } from 'firebase/firestore';
+import { collection, query, getDocs, getDoc, doc, orderBy, deleteDoc } from 'firebase/firestore';
 import { db } from '../../firebase';
 import ProfileCard from './ProfileCard';
 
@@ -20,8 +20,11 @@ const Profile = () => {
     const qUserPosts = query(userPostsRef, orderBy('createdAt', 'desc'));
     const userPostsSnapshot = await getDocs(qUserPosts);
 
+    console.log(userPostsSnapshot.docs[0].id);
     userPostsSnapshot.forEach((doc) => {
-      posts.push(doc.data());
+      // console.log(doc.id);
+      posts.push({ ...doc.data(), id: doc.id });
+      console.log('Posts', posts);
     });
     setUserPosts(posts);
   }, []);
@@ -36,14 +39,23 @@ const Profile = () => {
     getUserPosts();
   }, []);
 
+  const handleDelete = useCallback(async (e) => {
+    console.log(e.target);
+    // const deletePost = await deleteDoc(doc(db, 'profiles', uid, 'posts', e.target.value));
+  });
+
   return (
     <div className='flex w-full'>
       <ProfileCard userInfo={userInfo} uid={uid} />
       <div className='grid flex-grow card bg-base-300 rounded-box place-items-center'>
-        {userPosts?.map(({ uploadedPhoto, caption, displayName }, i) => (
+        {userPosts?.map(({ uploadedPhoto, caption, displayName, id }, i) => (
           <div key={i}>
+            <p>{id}</p>
             <div className='grid h-300 card bg-base-300 rounded-box place-items-center'>
               <div key={i} className='card lg:card-side bg-base-100 shadow-xl'>
+                <label onClick={handleDelete} className='btn btn-sm btn-circle absolute right-2 top-2'>
+                  ...
+                </label>
                 <figure>
                   <img className='object-contain h-60 w-60' src={`${uploadedPhoto}`} alt='pic' />
                 </figure>
