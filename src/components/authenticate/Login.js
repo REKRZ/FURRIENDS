e927ba0c8254fb8e2e103a3d751d7135ad33d0cb
Roy Neville <royneville@Roys-MBP.attlocal.net>
2022-07-05 12:32:16 -0700

    conditional navbar render

--- src/components/authenticate/Login.js
+++ src/components/authenticate/Login.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 import { useAuth } from '../../contexts/AuthContext';
 import SignOut from './SignOut';
 import { Link, useNavigate } from 'react-router-dom';
@@ -27,14 +27,27 @@ export default function Login() {
     // passwordRef.current.value = "";
   }
 
+  // const handleGoogleSignIn = useCallback(
+  //   async (e) => {
+  //     e.preventDefault();
+  //     setError('');
+  //     setLoading(true);
+  //     if (loading) {
+  //       await signInWithGoogle()
+  //         .then(() => setLoading(false))
+  //         .catch(() => setError('Failed to sign in'));
+  //     } else navigate('/home');
+  //     setLoading(false);
+  //   },
+  //   [loading]
+  // );
+
   async function handleGoogleSignIn(e) {
     e.preventDefault();
     try {
       setError('');
       setLoading(true);
       await signInWithGoogle();
-      // below line sends you to home page
-      navigate('/home');
     } catch {
       setError('Failed to sign in.');
     }
@@ -43,18 +56,11 @@ export default function Login() {
 
   return (
     <div className='h-screen flex flex-col items-center justify-center'>
-      {/* LEAVING THIS IN FOR NOW FOR DEVELOPMENT */}
-      {JSON.stringify(currentUser)}
       {/* THIS IS ERROR MESSAGE THAT WILL DISPLAY IF BAD LOGIN */}
       {error && (
         <div className='alert alert-warning shadow-lg max-w-fit'>
           <div>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              className='stroke-current flex-shrink-0 h-6 w-6'
-              fill='none'
-              viewBox='0 0 24 24'
-            >
+            <svg xmlns='http://www.w3.org/2000/svg' className='stroke-current flex-shrink-0 h-6 w-6' fill='none' viewBox='0 0 24 24'>
               <path
                 strokeLinecap='round'
                 strokeLinejoin='round'
@@ -72,34 +78,14 @@ export default function Login() {
           <strong>Login</strong>
         </h1>
         <label>Email</label>
-        <input
-          type='email'
-          placeholder='email...'
-          id='email'
-          name='email'
-          ref={emailRef}
-          className='mb-3'
-          required
-        ></input>
+        <input type='email' placeholder='email...' id='email' name='email' ref={emailRef} className='mb-3' required></input>
         <label>Password</label>
-        <input
-          type='password'
-          placeholder='password...'
-          id='password'
-          name='password'
-          ref={passwordRef}
-          className='mb-3'
-          required
-        ></input>
+        <input type='password' placeholder='password...' id='password' name='password' ref={passwordRef} className='mb-3' required></input>
 
         <button disabled={loading} className='btn mb-5' onClick={handleSubmit}>
           Sign in
         </button>
-        <button
-          disabled={loading}
-          className='btn mb-5'
-          onClick={handleGoogleSignIn}
-        >
+        <button disabled={loading} className='btn mb-5 ' onClick={handleGoogleSignIn}>
           Sign in with Google
         </button>
         <div className='flex justify-center text-sm'>
--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -50,80 +50,78 @@ export const Navbar = () => {
   return (
     <div className='navbar bg-base-300 '>
       <div className='flex-1 '>
-        <Link
-          className='btn btn-ghost mr-10 normal-case text-xl'
-          to='/home'
-          href='#'
-        >
-          <img
-            src='/images/logo.svg'
-            alt='logo'
-            className='object-scale-down h-12'
-          />
+        <Link className='btn btn-ghost mr-10 normal-case text-xl' to={currentUser ? '/home' : '/'} href='#'>
+          <img src='/images/logo.svg' alt='logo' className='object-scale-down h-12' />
         </Link>
         <div className='text-lg'>{`Welcome ${displayName}!`}</div>
       </div>
       <div className='flex-none '>
-        <ul className='menu menu-horizontal p-0 '>
-          <li>
-            <AddPost />
-          </li>
-          <li tabIndex='0' className='mx-2'>
-            <Link to='/home' href='#' className='btn btn-ghost'>
-              Chat
-              {/* <svg className='fill-current' xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24'>
-                <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
-              </svg> */}
-            </Link>
-            <ul className='p-2 shadow menu menu-compact dropdown-content bg-base-300 rounded-box w-45 z-40'>
-              {friends.length ? (
-                friends.map((friend) => (
-                  <li key={friend.id}>
-                    <Link to='/chatroom' state={{ from: friend.id }}>
-                      {friend.friendDisplayName}
-                    </Link>
-                  </li>
-                ))
-              ) : (
-                <li>Find Friends!</li>
-              )}
-            </ul>
-          </li>
-          <li>
-            <Link to='/map' href='#' className='btn btn-ghost'>
-              Map
-            </Link>
-          </li>
-        </ul>
+        {currentUser ? (
+          <ul className='menu menu-horizontal p-0 '>
+            <li>
+              <AddPost />
+            </li>
+
+            <li tabIndex='0' className='mx-2'>
+              <Link to='/home' href='#' className='btn btn-ghost'>
+                Chat
+                <svg className='fill-current' xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24'>
+                  <path d='M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z' />
+                </svg>
+              </Link>
+              <ul className='p-2 shadow menu menu-compact dropdown-content bg-base-300 rounded-box w-45 z-40'>
+                {friends.length ? (
+                  friends.map((friend) => (
+                    <li key={friend.id}>
+                      <Link to='/chatroom' state={{ from: friend.id }}>
+                        {friend.friendDisplayName}
+                      </Link>
+                    </li>
+                  ))
+                ) : (
+                  <li>Find Friends!</li>
+                )}
+              </ul>
+            </li>
+            <li>
+              <Link to='/map' href='#' className='btn btn-ghost'>
+                Map
+              </Link>
+            </li>
+          </ul>
+        ) : null}
       </div>
       <div className='flex-none gap-2 mx-2'>
-        <div className='form-control'>
-          {/* <input type='text' placeholder='Search' className='input input-bordered' /> */}
-        </div>
+        <div className='form-control'>{/* <input type='text' placeholder='Search' className='input input-bordered' /> */}</div>
         <div className='dropdown dropdown-end'>
           <label tabIndex='0' className='btn btn-ghost btn-circle avatar'>
             <div className='w-10 rounded-full'>
-              <img src={profilePic} alt='Profile Picture' />
+              <img src={currentUser ? profilePic : '/images/dogLogo.svg'} alt='Profile-Pic' />
             </div>
           </label>
-          <ul
-            tabIndex='0'
-            className='p-2 shadow menu menu-compact dropdown-content bg-base-300 rounded-box w-52'
-          >
-            <li>
-              <Link className='justify-between' to='/profile' href='#'>
-                Profile
-                <span className='badge'>New</span>
+          <ul tabIndex='0' className='p-2 shadow menu menu-compact dropdown-content bg-base-300 rounded-box w-52'>
+            {currentUser ? (
+              <div>
+                <li>
+                  <Link className='justify-between' to='/profile' href='#'>
+                    Profile
+                    <span className='badge'>New</span>
+                  </Link>
+                </li>
+                <li>
+                  <Link to='/home' href='#'>
+                    Settings
+                  </Link>
+                </li>
+                <li>
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              </div>
+            ) : (
+              <Link className='justify-between' to='/login'>
+                Login
               </Link>
-            </li>
-            <li>
-              <Link to='/home' href='#'>
-                Settings
-              </Link>
-            </li>
-            <li>
-              <button onClick={handleLogout}>Logout</button>
-            </li>
+            )}
           </ul>
         </div>
       </div>
