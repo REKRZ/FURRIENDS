5b36f35d40bae46a0132ddae7879e893622f1200
Ed Geng <egeng01@gmail.com>
2022-07-05 22:01:22 -0400

    added delete handler for profile posts

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -14,19 +14,35 @@ const Profile = () => {
   const userInfoRef = doc(db, 'profiles', uid);
   const qUserInfo = query(userInfoRef);
 
-  const getUserPosts = useCallback(async () => {
-    let posts = [];
-    const userPostsRef = collection(db, 'profiles', uid, 'posts');
-    const qUserPosts = query(userPostsRef, orderBy('createdAt', 'desc'));
-    const userPostsSnapshot = await getDocs(qUserPosts);
+  // const getUserPosts = useCallback(async () => {
+  //   let posts = [];
+  //   const userPostsRef = collection(db, 'profiles', uid, 'posts');
+  //   const qUserPosts = query(userPostsRef, orderBy('createdAt', 'desc'));
+  //   const userPostsSnapshot = await getDocs(qUserPosts);
 
-    console.log(userPostsSnapshot.docs[0].id);
-    userPostsSnapshot.forEach((doc) => {
-      // console.log(doc.id);
-      posts.push({ ...doc.data(), id: doc.id });
-      console.log('Posts', posts);
-    });
-    setUserPosts(posts);
+  //   userPostsSnapshot.forEach((doc) => {
+  //     // console.log(doc.id);
+  //     posts.push({ ...doc.data(), id: doc.id });
+  //     console.log('Posts', posts);
+  //   });
+  //   setUserPosts(posts);
+  // }, [userPosts]);
+
+  useEffect(() => {
+    const getUserPosts = async () => {
+      let posts = [];
+      const userPostsRef = collection(db, 'profiles', uid, 'posts');
+      const qUserPosts = query(userPostsRef, orderBy('createdAt', 'desc'));
+      const userPostsSnapshot = await getDocs(qUserPosts);
+
+      userPostsSnapshot.forEach((doc) => {
+        // console.log(doc.id);
+        posts.push({ ...doc.data(), id: doc.id });
+        console.log('Posts', posts);
+      });
+      setUserPosts(posts);
+    };
+    getUserPosts();
   }, []);
 
   useEffect(() => {
@@ -36,41 +52,43 @@ const Profile = () => {
     };
 
     getUserInfo();
-    getUserPosts();
   }, []);
 
-  const handleDelete = useCallback(async (e) => {
-    console.log(e.target);
-    // const deletePost = await deleteDoc(doc(db, 'profiles', uid, 'posts', e.target.value));
-  });
+  const handleDelete = async (id) => {
+    await deleteDoc(doc(db, 'profiles', uid, 'posts', id));
+  };
 
   return (
     <div className='flex w-full'>
       <ProfileCard userInfo={userInfo} uid={uid} />
       <div className='grid flex-grow card bg-base-300 rounded-box place-items-center'>
-        {userPosts?.map(({ uploadedPhoto, caption, displayName, id }, i) => (
-          <div key={i}>
-            <p>{id}</p>
-            <div className='grid h-300 card bg-base-300 rounded-box place-items-center'>
-              <div key={i} className='card lg:card-side bg-base-100 shadow-xl'>
-                <label onClick={handleDelete} className='btn btn-sm btn-circle absolute right-2 top-2'>
-                  ...
-                </label>
-                <figure>
-                  <img className='object-contain h-60 w-60' src={`${uploadedPhoto}`} alt='pic' />
-                </figure>
-                <div className='card-body'>
-                  <h6 className='card-title'>{caption}</h6>
-                  <p>If a dog chews shoes whose shoes does he choose??</p>
-                  <div className='card-actions justify-end'>
-                    <div className='badge badge-outline'>{displayName}</div>
+        {userPosts.length ? (
+          userPosts.map(({ uploadedPhoto, caption, displayName, id }, i) => (
+            <div key={i}>
+              <p>{id}</p>
+              <div className='grid h-300 card bg-base-300 rounded-box place-items-center'>
+                <div key={i} className='card lg:card-side bg-base-100 shadow-xl'>
+                  <button onClick={() => handleDelete(id)} className='btn btn-sm btn-circle absolute right-2 top-2'>
+                    ...
+                  </button>
+                  <figure>
+                    <img className='object-contain h-60 w-60' src={`${uploadedPhoto}`} alt='pic' />
+                  </figure>
+                  <div className='card-body'>
+                    <h6 className='card-title'>{caption}</h6>
+                    <p>If a dog chews shoes whose shoes does he choose??</p>
+                    <div className='card-actions justify-end'>
+                      <div className='badge badge-outline'>{displayName}</div>
+                    </div>
                   </div>
                 </div>
               </div>
+              <div className='divider'></div>
             </div>
-            <div className='divider'></div>
-          </div>
-        ))}
+          ))
+        ) : (
+          <h1>No Posts at this time</h1>
+        )}
       </div>
     </div>
   );
