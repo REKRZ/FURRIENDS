24fe2198e145c9298c293665d3666559c5389b07
Kenji Zhang <96086699+kenjizhang@users.noreply.github.com>
2022-07-08 08:12:48 -0700

    added friend profiles

---
+++ src/components/profile/FriendProfileCard.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+const ProfileCard = ({ userInfo, uid }) => {
+  const { photoURL, displayName, bio } = userInfo;
+
+  return (
+    <div className='card w-60 h-96 bg-base-100 shadow-xl items-center'>
+      <div className='avatar pt-10'>
+        <div className='w-40 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2'>
+          <img
+            src={photoURL}
+            alt='profileImage'
+            className='btn-circle avatar '
+          />
+        </div>
+      </div>
+      <div className='card-body items-center text-center'>
+        <h2 className='card-title'>{displayName}</h2>
+        <p>{bio}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
