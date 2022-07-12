235b2d4592f159a4044c03566be19c11d1b46422
Kenji Zhang <96086699+kenjizhang@users.noreply.github.com>
2022-07-12 12:25:31 -0700

    fixed edit captions

--- src/components/profile/EditPost.js
+++ src/components/profile/EditPost.js
@@ -22,16 +22,25 @@ const EditPost = ({ caption, id }) => {
       console.log(err);
     }
   };
-
   return (
     <>
-      <label htmlFor='edit-post-modal' className='btn m-1 btn-sm btn-circle modal-button'>
+      <label
+        htmlFor={`edit-post-modal-${id}}`}
+        className='btn m-1 btn-sm btn-circle modal-button'
+      >
         <BsFillPencilFill />
       </label>
-      <input type='checkbox' id='edit-post-modal' className='modal-toggle' />
+      <input
+        type='checkbox'
+        id={`edit-post-modal-${id}}`}
+        className='modal-toggle'
+      />
       <div className='modal modal-bottom sm:modal-middle'>
         <div className='modal-box'>
-          <label htmlFor='edit-post-modal' className='btn btn-sm btn-circle absolute right-2 top-2'>
+          <label
+            htmlFor={`edit-post-modal-${id}}`}
+            className='btn btn-sm btn-circle absolute right-2 top-2'
+          >
             ✕
           </label>
           <h3 className='font-bold text-lg pb-4 text-center'>Edit Caption</h3>
@@ -40,12 +49,22 @@ const EditPost = ({ caption, id }) => {
               <label className='input-group flex flex-col items-center space-y-4'>
                 <div className='flex flex-row'>
                   <span>Caption</span>
-                  <input ref={captionRef} type='text' placeholder={caption} className='input input-bordered' />
+                  <input
+                    ref={captionRef}
+                    type='text'
+                    defaultValue={caption}
+                    className='input input-bordered'
+                    size={40}
+                  />
                 </div>
               </label>
             </div>
             <div className='modal-action'>
-              <button type='submit' htmlFor='edit-post-modal' className='btn btn-m1'>
+              <button
+                type='submit'
+                htmlFor='edit-post-modal'
+                className='btn btn-m1'
+              >
                 <label htmlFor='edit-post-modal'>Submit</label>
               </button>
             </div>
